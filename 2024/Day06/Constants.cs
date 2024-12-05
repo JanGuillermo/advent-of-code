8255967eefd380068154f49175898745f3fcb2ba
JanGuillermo <github.reversion666@passinbox.com>
2024-12-04 23:05:23 -0800

    Add Day06 project

---
+++ 2024/Day06/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace _2024.Day06;
+
+internal class Constants
+{
+
+}
---
+++ 2024/Day06/Solution.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+
+namespace _2024.Day06;
+
+internal class Solution : BaseSolution
+{
+    public Solution() : base(2024, 6)
+    {
+
+    }
+
+    public override int SolvePartOne()
+    {
+        return 0;
+    }
+
+    public override int SolvePartTwo()
+    {
+        return 0;
+    }
+}
