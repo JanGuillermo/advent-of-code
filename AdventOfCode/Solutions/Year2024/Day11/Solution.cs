b6bc864630bdc85e736f78773c2dc9a88429b076
JanGuillermo <github.reversion666@passinbox.com>
2024-12-10 21:51:11 -0800

    Add 2024's Day 11  solution

--- AdventOfCode/Solutions/Year2024/Day11/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day11/Solution.cs
@@ -1,20 +1,57 @@
 namespace AdventOfCode.Solutions.Year2024.Day11;
 
 /// <summary>
-/// Day 11
+/// Day 11: Plutonian Pebbles.
 /// <see cref="https://adventofcode.com/2024/day/11"/>
 /// </summary>
 internal class Solution : SolutionBase
 {
-    public Solution() : base(2024, 11) { }
+    List<long> stones = [];
+    Dictionary<(long, int), long> memo = [];
+
+    public Solution() : base(2024, 11)
+    {
+        ProcessInput(Input);
+    }
 
     public override object SolvePartOne()
     {
-        return 0;
+        return stones.Sum(stone => Blink(stone, 25));
     }
 
     public override object SolvePartTwo()
     {
-        return 0;
+        return stones.Sum(stone => Blink(stone, 75));
+    }
+
+    private long Blink(long stone, int blinks)
+    {
+        if (blinks == 0) return 1;
+        if (stone == 0) return Blink(1, blinks - 1);
+        if (memo.TryGetValue((stone, blinks), out long result)) return result;
+
+        string stoneStr = stone.ToString();
+
+        if (stoneStr.Length % 2 == 0)
+        {
+            int mid = stoneStr.Length / 2;
+            long leftHalf = long.Parse(stoneStr.Substring(0, mid));
+            long rightHalf = long.Parse(stoneStr.Substring(mid));
+
+            result = Blink(leftHalf, blinks - 1) + Blink(rightHalf, blinks - 1);
+        }
+        else
+        {
+            result = Blink(stone * 2024, blinks - 1);
+        }
+
+        memo[(stone, blinks)] = result;
+
+        return result;
+    }
+
+    private void ProcessInput(string input)
+    {
+        stones = input.Trim().Split(" ").Select(long.Parse).ToList();
     }
 }
