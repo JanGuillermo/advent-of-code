3d2ddf2ae37141c299b6dfa601e9f6acae5e3752
JanGuillermo <github.reversion666@passinbox.com>
2024-12-15 13:53:37 -0800

    Refactor 2024's Day 1 solution

--- AdventOfCode/Solutions/Year2024/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day01/Solution.cs
@@ -9,20 +9,10 @@ namespace AdventOfCode.Solutions.Year2024.Day01;
 /// </summary>
 internal class Solution : SolutionBase
 {
-    public List<int> Locations1 = [];
-    public List<int> Locations2 = [];
+    private List<int> Locations1 = [];
+    private List<int> Locations2 = [];
 
-    public Solution() : base(2024, 01)
-    {
-        foreach (Match match in new Regex(@"(\d{5})   (\d{5})").Matches(Input))
-        {
-            Locations1.Add(int.Parse(match.Groups[1].Value));
-            Locations2.Add(int.Parse(match.Groups[2].Value));
-        }
-
-        Locations1.Sort();
-        Locations2.Sort();
-    }
+    public Solution() : base(2024, 01) { }
 
     public override object SolvePartOne()
     {
@@ -38,5 +28,17 @@ internal class Solution : SolutionBase
             .ToDictionary(group => group.Key, group => group.Count());
 
         return Locations1.Sum(value => frequencies.GetValueOrDefault(value) * value);
+    }
+
+    protected override void ProcessInput()
+    {
+        foreach (Match match in new Regex(@"(\d{5})   (\d{5})").Matches(Input))
+        {
+            Locations1.Add(int.Parse(match.Groups[1].Value));
+            Locations2.Add(int.Parse(match.Groups[2].Value));
+        }
+
+        Locations1.Sort();
+        Locations2.Sort();
     }
 }
