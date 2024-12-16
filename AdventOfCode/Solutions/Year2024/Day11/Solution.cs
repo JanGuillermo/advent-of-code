89419ac2c99af1618dddf26f8f03c81987f6b74c
JanGuillermo <github.reversion666@passinbox.com>
2024-12-15 16:51:22 -0800

    Refactor 2024's Day 11 solution

--- AdventOfCode/Solutions/Year2024/Day11/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day11/Solution.cs
@@ -7,29 +7,26 @@ namespace AdventOfCode.Solutions.Year2024.Day11;
 /// </summary>
 internal class Solution : SolutionBase
 {
-    List<long> stones = [];
-    Dictionary<(long, int), long> memo = [];
+    private List<long> Stones = [];
+    private Dictionary<(long, int), long> Memo = [];
 
-    public Solution() : base(2024, 11)
-    {
-        ProcessInput(Input);
-    }
+    public Solution() : base(2024, 11) { }
 
     public override object SolvePartOne()
     {
-        return stones.Sum(stone => Blink(stone, 25));
+        return Stones.Sum(stone => Blink(stone, 25));
     }
 
     public override object SolvePartTwo()
     {
-        return stones.Sum(stone => Blink(stone, 75));
+        return Stones.Sum(stone => Blink(stone, 75));
     }
 
     private long Blink(long stone, int blinks)
     {
+        if (Memo.TryGetValue((stone, blinks), out long result)) return result;
         if (blinks == 0) return 1;
         if (stone == 0) return Blink(1, blinks - 1);
-        if (memo.TryGetValue((stone, blinks), out long result)) return result;
 
         string stoneStr = stone.ToString();
 
@@ -46,13 +43,13 @@ internal class Solution : SolutionBase
             result = Blink(stone * 2024, blinks - 1);
         }
 
-        memo[(stone, blinks)] = result;
+        Memo[(stone, blinks)] = result;
 
         return result;
     }
 
-    private void ProcessInput(string input)
+    protected override void ProcessInput()
     {
-        stones = input.Trim().Split(" ").Select(long.Parse).ToList();
+        Stones = Input.Split(" ").Select(long.Parse).ToList();
     }
 }
