068353c45e99f69384fde095b41b5a6db6396e09
JanGuillermo <github.reversion666@passinbox.com>
2024-12-15 16:31:59 -0800

    Refactor 2024's Day 8 solution

--- AdventOfCode/Solutions/Year2024/Day08/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day08/Solution.cs
@@ -1,4 +1,7 @@
-﻿namespace AdventOfCode.Solutions.Year2024.Day08;
+﻿using AdventOfCode.Solutions.Objects;
+using AdventOfCode.Solutions.Utils;
+
+namespace AdventOfCode.Solutions.Year2024.Day08;
 
 /// <summary>
 /// <see href="https://adventofcode.com/2024/day/8">
@@ -7,30 +10,105 @@
 /// </summary>
 internal class Solution : SolutionBase
 {
-    public Dictionary<Position, char> Map = new();
-    public Dictionary<char, List<Position>> Frequencies = new();
-    public int MapWidth = 0;
-    public int MapHeight = 0;
+    private const char EMPTY = '.';
 
-    public const char Empty = '.';
+    private Dictionary<Position, char> Map = [];
+    private Dictionary<char, List<Position>> Frequencies = [];
+    private int MapCols = 0;
+    private int MapRows = 0;
 
-    public Solution() : base(2024, 08)
+    public Solution() : base(2024, 08, true) { }
+
+    public override object SolvePartOne()
     {
-        string[] lines = Input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        return Frequencies
+            .SelectMany(frequency => FindAntinodes(Map, frequency.Key, frequency.Value, MapRows, MapCols))
+            .Distinct()
+            .Count();
+    }
 
-        for (int row = 0; row < lines.Length; row++)
+    public override object SolvePartTwo()
+    {
+        return Frequencies
+            .SelectMany(frequency => FindAntinodes(Map, frequency.Key, frequency.Value, MapRows, MapCols, isUpdatedModel: true))
+            .Distinct()
+            .Count();
+    }
+
+    private static HashSet<Position> FindAntinodes(Dictionary<Position, char> map, char frequency, List<Position> positions, int mapRows, int mapCols, bool isUpdatedModel = false)
+    {
+        HashSet<Position> antinodes = [];
+
+        for (int i = 0; i < positions.Count - 1; i++)
         {
-            for (int col = 0; col < lines[row].Length; col++)
+            Position position = positions[i];
+
+            for (int j = i + 1; j < positions.Count; j++)
+            {
+                Position otherPosition = positions[j];
+
+                int rowDiff = position.Row - otherPosition.Row;
+                int colDiff = position.Col - otherPosition.Col;
+
+                if (isUpdatedModel)
+                {
+                    AddAntinodes(mapRows, mapCols, antinodes, position, rowDiff, colDiff);
+                    AddAntinodes(mapRows, mapCols, antinodes, position, -rowDiff, -colDiff);
+                    AddAntinodes(mapRows, mapCols, antinodes, otherPosition, -rowDiff, -colDiff);
+                    AddAntinodes(mapRows, mapCols, antinodes, otherPosition, rowDiff, colDiff);
+                }
+                else
+                {
+                    AddAntinodeIfValid(map, mapRows, mapCols, antinodes, position, rowDiff, colDiff, frequency);
+                    AddAntinodeIfValid(map, mapRows, mapCols, antinodes, otherPosition, -rowDiff, -colDiff, frequency);
+                }
+            }
+        }
+
+        return antinodes;
+    }
+
+    private static void AddAntinodeIfValid(Dictionary<Position, char> map, int mapRows, int mapCols, HashSet<Position> antinodes, Position start, int rowDiff, int colDiff, char frequency)
+    {
+        Position antiNode = start.Move(rowDiff, colDiff);
+
+        if (antiNode.IsInBounds(mapRows, mapCols) && map[antiNode] != frequency)
+        {
+            antinodes.Add(antiNode);
+        }
+    }
+
+    private static void AddAntinodes(int mapRows, int mapCols, HashSet<Position> antinodes, Position start, int rowDiff, int colDiff)
+    {
+        Position antiNode = start.Move(rowDiff, colDiff);
+
+        while (antiNode.IsInBounds(mapRows, mapCols))
+        {
+            antinodes.Add(antiNode);
+            antiNode = antiNode.Move(rowDiff, colDiff);
+        }
+    }
+
+    protected override void ProcessInput()
+    {
+        string[] lines = InputUtils.SplitIntoLines(Input);
+
+        MapRows = lines.Length;
+        MapCols = lines[0].Length;
+
+        for (int row = 0; row < MapRows; row++)
+        {
+            for (int col = 0; col < MapCols; col++)
             {
                 char cellValue = lines[row][col];
                 Position position = new Position(row, col);
                 Map[position] = cellValue;
 
-                if (cellValue != Empty)
+                if (cellValue != EMPTY)
                 {
                     if (!Frequencies.TryGetValue(cellValue, out List<Position>? positions))
                     {
-                        positions = new List<Position>();
+                        positions = [];
                         Frequencies[cellValue] = positions;
                     }
 
@@ -38,88 +116,5 @@ internal class Solution : SolutionBase
                 }
             }
         }
-
-        MapWidth = lines[0].Length;
-        MapHeight = lines.Length;
     }
-
-    public override object SolvePartOne()
-    {
-        HashSet<Position> antinodes = new();
-
-        foreach ((char frequency, List<Position> positions) in Frequencies)
-        {
-            for (int i = 0; i < positions.Count - 1; i++)
-            {
-                Position position = positions[i];
-
-                for (int j = i + 1; j < positions.Count; j++)
-                {
-                    Position otherPosition = positions[j];
-
-                    int rowDiff = position.Row - otherPosition.Row;
-                    int colDiff = position.Col - otherPosition.Col;
-
-                    AddAntinodeIfValid(antinodes, position, rowDiff, colDiff, frequency);
-                    AddAntinodeIfValid(antinodes, otherPosition, -rowDiff, -colDiff, frequency);
-                }
-            }
-        }
-
-        return antinodes.Count;
-    }
-
-    public override object SolvePartTwo()
-    {
-        HashSet<Position> antinodes = new();
-
-        foreach ((char frequency, List<Position> positions) in Frequencies)
-        {
-            for (int i = 0; i < positions.Count - 1; i++)
-            {
-                Position position = positions[i];
-
-                for (int j = i + 1; j < positions.Count; j++)
-                {
-                    Position otherPosition = positions[j];
-
-                    int rowDiff = position.Row - otherPosition.Row;
-                    int colDiff = position.Col - otherPosition.Col;
-
-                    AddAntinodes(antinodes, position, rowDiff, colDiff);
-                    AddAntinodes(antinodes, position, -rowDiff, -colDiff);
-                    AddAntinodes(antinodes, otherPosition, -rowDiff, -colDiff);
-                    AddAntinodes(antinodes, otherPosition, rowDiff, colDiff);
-                }
-            }
-        }
-
-        return antinodes.Count;
-    }
-
-    private void AddAntinodeIfValid(HashSet<Position> antinodes, Position position, int rowDiff, int colDiff, char frequency)
-    {
-        Position antiNode = position.Move(rowDiff, colDiff);
-        if (antiNode.IsInBounds(MapHeight, MapWidth) && Map[antiNode] != frequency)
-        {
-            antinodes.Add(antiNode);
-        }
-    }
-
-    private void AddAntinodes(HashSet<Position> antinodes, Position start, int rowDiff, int colDiff)
-    {
-        Position antiNode = start.Move(rowDiff, colDiff);
-
-        while (antiNode.IsInBounds(MapHeight, MapWidth))
-        {
-            antinodes.Add(antiNode);
-            antiNode = antiNode.Move(rowDiff, colDiff);
-        }
-    }
-}
-
-internal record Position(int Row, int Col)
-{
-    public Position Move(int row, int col) => new Position(Row + row, Col + col);
-    public bool IsInBounds(int rows, int cols) => Row >= 0 && Row < rows && Col >= 0 && Col < cols;
 }
